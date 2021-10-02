66bbd506a0ddf9e7e96538ac4b245812795f9735
mahbha <43632182+mahbha@users.noreply.github.com>
2021-10-02 19:31:14 +0530

    Added code to update time on button click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let now = new Date().toLocaleTimeString();
+  let [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
